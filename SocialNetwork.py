454e78d33f07b0385132156db2d23d2c00050d88
yairMar1 <156588104+yairMar1@users.noreply.github.com>
2024-03-03 14:42:53 +0200

    Add files via upload
    3.3.24 14:42

---
+++ SocialNetwork.py
@@ -0,0 +1,54 @@
+from user import User
+
+'''
+I added an error alert that will pop up to the user in the sign_up function.
+It will be written to the user what errors he made:
+"There is already such a username on the network"
+or
+"The password is not the right length"
+'''
+
+
+class SocialNetwork:
+    __instance = None
+    __is_initialized = False
+
+    def __new__(cls, name: str):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    def __init__(self, name: str):
+        if self.__is_initialized:
+            return
+        self.name = name
+        self.users = dict()
+        self.__is_initialized = True
+        print(f"The social network" + " " + self.name + " " + "was created!")
+
+    def __str__(self):
+        result = f"{self.name} social network:\n"
+        for user in self.users.values():
+            result += str(user) + "\n"
+        return result
+
+    def sign_up(self, username, password):
+        if username not in self.users:
+            if 4 <= len(password) & len(password) <= 8:
+                new_user = User(username, password, True)
+                self.users[new_user.username] = new_user
+                return new_user
+            else:
+                raise ValueError("Password length should be between 4 and 8 characters")
+        else:
+            raise ValueError(f"Username {username} is already in use")
+
+    def log_out(self, username):
+        if username in self.users:
+            self.users[username].connect = False
+        print(f"{username} disconnected")
+
+    def log_in(self, username, password):
+        if username in self.users and self.users[username].password == password:
+            self.users[username].connect = True
+        print(f"{username} connected")
